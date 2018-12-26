12e601063eb545acf9df05f049f9cabbbc19884a
Swati Singh <amenta10203066@gmail.com>
2018-12-26 17:46:01 +0000

    Fashion_model

---
+++ fashion_model.py
@@ -0,0 +1,7 @@
+# Here you should define your network.
+# As with MNIST, each image is 28x28 which is a total of 784 pixels, and there are 10 classes.
+# You should include at least one hidden layer. We suggest you use ReLU activations for the layers and to return the logits or log-softmax from the forward pass.
+# It's up to you how many layers you add and the size of those layers.
+
+# TODO: Define your network architecture here
+
